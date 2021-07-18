0c6700f69a30d7bfbfdb1ea9d6875d17b6a2d028
Anatoli Belchikov <anatoli.belchikov@market.com>
2021-07-18 19:39:51 +0300

    Added artifacts detection starter

--- eeg_events.py
+++ eeg_events.py
@@ -5,12 +5,13 @@ import pandas as pd
 
 def get_paths(name):
     return (
+        f"./valid-data/processed/{name}-unprocessed.raw.fif",
         f"./valid-data/processed/events-{name}.csv",
         f"./valid-data/processed/{name}.raw.fif"
     )
 
-def save_events(raw, events_df, name):
-    events_path, raw_path = get_paths(name)
+def save_events(raw, raw_alpha, events_df, name):
+    raw_path, events_path, raw_alpha_path = get_paths(name)
     
     record_date = raw.info["start_time"].split()[0]
     record_start_time = dt.datetime.strptime(raw.info["start_time"], "%Y-%m-%d %H:%M:%S")
@@ -19,6 +20,7 @@ def save_events(raw, events_df, name):
     events_df["timestamp"] = (events_df["datetime"] - record_start_time).dt.total_seconds().astype(int)
     
     raw.save(raw_path, overwrite=True)
+    raw_alpha.save(raw_alpha_path, overwrite=True)
     events_df.to_csv(events_path, index=False)
     
 def iterate_events(name, duration=1):
